d6a2d91d11a9cbb08e720d9d35196a0f723ab2df
ARTEMISA11 <camila.mantilla@udea.edu.co>
2026-03-22 19:58:42 -0500

    Creamos sh descarga CSV, con encabezado

---
+++ pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo = " Iniciando descarga archivo CSV"
+
+URL="https://skyserver.sdss.org/dr18/SkyServerWS/SearchTools/SqlSearch?format=csv&cmd=SELECT%20TOP%2050000%20s.class,s.z,p.g,p.r%20FROM%20SpecObj%20s%20JOIN%20PhotoObj%20p%20ON%20s.bestObjID=p.objID%20WHERE%20s.class=%27QSO%27%20OR%20s.class=%27GALAXY%27"
+
+wget -q -O datos_sdss.csv "$URL"
+
+echo = "Catalogo SDSS DR18 descargado" 
+
+head -n 5 datos_sdss.csv
